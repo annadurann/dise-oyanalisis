8ffde35ed42061145b1d8b0e3698f0a07a7833bd
annadurann <143562373+annadurann@users.noreply.github.com>
2024-01-30 12:50:06 -0600

    programa con datos
    programa con datos pero con entradas de texto

---
+++ programadatos2.py
@@ -0,0 +1,5 @@
+nombre = input("¿Cual es tu nombre? ")
+nocontrol = input("Ingresa tu número de control: ")
+
+print("El nombre del usuario es: "+ nombre)
+print("El número de control del usuario es: " + nocontrol)
